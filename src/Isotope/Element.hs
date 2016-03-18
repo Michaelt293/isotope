b34876e0b80bff7e71cb9694be85b02bda02e431
Michaelt293 <Michaelt293@gmail.com>
2016-03-18 18:50:38 +1100

    New modules added

---
+++ src/Isotope/Element.hs
@@ -0,0 +1,53 @@
+module Element
+    ( elementMostAbundantIsotope
+    , elementMonoisotopicMass
+    , elementNominalMass
+    , elementIsotopicMasses
+    , elementIntegerMasses
+    , elementAverageAtomicMass
+    , elementIsotopicAbundances
+    , massNumber
+    ) where
+
+import Periodic
+import Data.List           (elemIndex)
+import Data.Maybe          (fromJust)
+
+-- |Returns the most abundant naturally-occurring isotope for an eleElementment.
+elementMostAbundantIsotope :: Element -> Isotope
+elementMostAbundantIsotope e = isotopeList !! indexOfIsotope
+    where isotopeList = isotopes' e
+          abundances = isotopicAbundance <$> isotopeList
+          indexOfIsotope = fromJust $ elemIndex (maximum abundances) abundances
+
+-- |Monoisotopic mass for an element. Monoisotopic mass is the exact mass of the
+-- most abundant isotope.
+elementMonoisotopicMass :: Element -> MonoisotopicMass
+elementMonoisotopicMass = isotopicMass . elementMostAbundantIsotope
+
+-- |Nominal mass for an element. Nominal mass is the integer mass of the
+-- most abundant isotope.
+elementNominalMass :: Element -> NominalMass
+elementNominalMass = massNumber . nucleons . elementMostAbundantIsotope
+
+-- |Exact masses for all naturally-occurring isotopes for an element.
+elementIsotopicMasses :: Element -> [IsotopicMass]
+elementIsotopicMasses e = isotopicMass <$> isotopes' e
+
+-- |Integer masses for all naturally-occurring isotopes for an element.
+elementIntegerMasses :: Element -> [IntegerMass]
+elementIntegerMasses e = massNumber . nucleons <$> isotopes' e
+
+-- |Average atomic mass for an element.
+elementAverageAtomicMass :: Element -> AverageAtomicMass
+elementAverageAtomicMass e = sum [isotopicMass x * isotopicAbundance x |
+                             x <- isotopes' e]
+
+-- |Isotope abundances for all naturally-occurring isotopes for an element.
+elementIsotopicAbundances :: Element -> [IsotopicAbundance]
+elementIsotopicAbundances e = isotopicAbundance <$> isotopes' e
+
+-- |Mass number for an isotope. Mass number is the number of protons plus the
+-- number of neutrons.
+massNumber :: (ProtonNumber, NeutronNumber) -> MassNumber
+massNumber (protonNum, neutronNum) = protonNum + neutronNum
---
+++ src/Isotope/Molecule.hs
@@ -0,0 +1,104 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
+module Molecule where
+
+import Text.Megaparsec
+import Text.Megaparsec.String
+import qualified Text.Megaparsec.Lexer as L
+import Data.String
+import Control.Applicative
+import ElementIsotopes
+import Periodic
+import Data.Map hiding (map)
+
+type MolecularFormula = ElementSymbolMap Int
+
+elemSym :: Parser ElementSymbol
+elemSym = elemSym' <?> "element symbol"
+    where elemSym' = do
+                  upper <- upperChar
+                  lower <- optional lowerChar
+                  return $ case lower of
+                                Nothing -> read [upper]
+                                Just lower' -> read [upper, lower']
+
+elemSymNum :: Parser MolecularFormula
+elemSymNum = do
+    sym <- elemSym
+    num <- optional L.integer
+    return $ case num of
+                  Nothing -> mkElementSymbolMap [(sym, 1)]
+                  Just num' -> mkElementSymbolMap [(sym, fromIntegral num')]
+
+molecularFormula :: Parser MolecularFormula
+molecularFormula = do
+    formulas <- many elemSymNum
+    return $ mconcat formulas
+
+emptyFormula :: MolecularFormula
+emptyFormula = mkElementSymbolMap []
+
+renderFormula :: (Eq a, Num a, Show a) => ElementSymbolMap a -> String
+renderFormula f = foldMapWithKey foldfunc (getSymbolMap f)
+                      where foldfunc sym num = show sym ++ if num == 1
+                                                              then ""
+                                                              else show num
+
+instance IsString MolecularFormula where
+    fromString s = case parse (molecularFormula <* eof) "" s of
+                        Left err -> error $ "Could not parse molecular formula: " ++ show err
+                        Right v  -> v
+
+instance Monoid MolecularFormula where
+    mempty = emptyFormula
+    mappend = (|+|)
+
+class MolecularFormulae a where
+    getFormula      :: a -> MolecularFormula
+    getMaybeFormula :: a -> Maybe MolecularFormula
+
+class FormulaMult a b c | a b -> c where
+    (|*|) :: a -> b -> c
+
+(|+|) :: Num a => ElementSymbolMap a -> ElementSymbolMap a -> ElementSymbolMap a
+(|+|) = combineSymbolMaps (+)
+
+(|-|) :: Num a => ElementSymbolMap a -> ElementSymbolMap a -> ElementSymbolMap a
+(|-|) = combineSymbolMaps (-)
+
+combineSymbolMaps :: (a -> a -> a) -> ElementSymbolMap a -> ElementSymbolMap a -> ElementSymbolMap a
+combineSymbolMaps f m1 m2 = ElementSymbolMap $
+                                unionWith f (getSymbolMap m1) (getSymbolMap m2)
+
+infixl 6 |+|
+infixl 7 |*|
+infixl 6 |-|
+
+instance FormulaMult MolecularFormula Int MolecularFormula where
+    (|*|) = multMolecularFormula
+
+instance FormulaMult Int MolecularFormula MolecularFormula where
+    (|*|) = flip multMolecularFormula
+
+multMolecularFormula :: Num a => ElementSymbolMap a -> a -> ElementSymbolMap a
+multMolecularFormula m n = (n *) <$> m
+
+monoisotopicMassFormula :: MolecularFormula -> IsotopicMass
+monoisotopicMassFormula = getFormulaSum monoisotopicMass
+
+averageMassFormula :: MolecularFormula -> IsotopicMass
+averageMassFormula = getFormulaSum averageAtomicMass
+
+nominalMassFormula :: MolecularFormula -> IntegerMass
+nominalMassFormula = getFormulaSum nominalMass
+
+getFormulaSum :: (Num a, Integral b) => (ElementSymbol -> a) -> ElementSymbolMap b -> a
+getFormulaSum f m = sum $ foldFunc f symbolMap <$> keys symbolMap
+    where symbolMap = getSymbolMap m
+          foldFunc f' m' e = f' e * fromIntegral (m' ! e)

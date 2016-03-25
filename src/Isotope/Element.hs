8816c52e2093a5a673fa0859f6c4ca2fed160ad5
Michaelt293 <Michaelt293@gmail.com>
2016-03-25 12:48:30 +1100

    Documentation added

--- src/Isotope/Element.hs
+++ src/Isotope/Element.hs
@@ -1,3 +1,21 @@
+{-|
+Module      : Isotopic.Element
+Description : Contains most of the data type declarations used in the Isotope
+              library.
+Copyright   : Michael Thomas
+License     : GPL-3
+Maintainer  : Michael Thomas <Michaelt293@gmail.com>
+Stability   : Experimental
+
+This module defines the majority of the types used in the Isotope library. A
+large number of type synonyms are provided to improve readability. Of
+particular importance are the 'Isotope', 'Element', 'ElementSymbol' and
+'ElementSymbolMap' types. 'ElementSymbol' is an enumeration type of all the
+element symbols used in the Isotopes library. 'ElementSymbolMap' is a
+polymorphic data type which maps an 'ElementSymbol' to a value of some type.
+An 'ElementSymbolMap' is used in "Isotope.Periodic" to provide a mapping to
+values of type 'Element'. (The type 'Element' has a field of list of 'Isotope'.)
+-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE DeriveFoldable #-}
@@ -60,7 +78,7 @@ type ProtonNumber      = AtomicNumber
 -- | Neutron number (i.e., the number of neutrons) for an element.
 type NeutronNumber     = Int
 
--- | Type synonym for a pair containing ProtonNumber and neutronNumber.
+-- | Type synonym for a pair containing 'ProtonNumber' and 'NeutronNumber'.
 type Nucleons          = (ProtonNumber, NeutronNumber)
 
 -- | The number of protons plus the number of neutrons (i.e., proton number +
@@ -84,14 +102,15 @@ type NominalMass       = Int
 -- naturally-occurring abundances.
 type AverageMass       = Double
 
--- | An Isotope has three parameters; Nucleons, IsotopeMass and Isotopic
--- abundance.
+-- | An 'Isotope' has three parameters; 'Nucleons', 'IsotopeMass' and
+-- 'IsotopicAbundance'.
 data Isotope = Isotope { nucleons          :: Nucleons
                        , isotopicMass      :: IsotopicMass
                        , isotopicAbundance :: IsotopicAbundance
                        } deriving (Show, Eq, Ord)
 
--- | An Element has three parameters; AtomicNumber, ElementName and [Isotope].
+-- | An 'Element' has three parameters; 'AtomicNumber', 'ElementName' and
+-- list of 'Isotope'.
 data Element = Element { atomicNumber' :: AtomicNumber
                        , elementName'  :: ElementName
                        , isotopes'     :: [Isotope]
@@ -111,40 +130,39 @@ data ElementSymbol = H  | He | Li | Be | B  | C  | N  | O  | F  | Ne | Na | Mg |
 elementSymbolList :: [ElementSymbol]
 elementSymbolList = [H .. U]
 
--- | A ElementSymbolMap is a polymorphic datatype mapping an ElementSymbol to
--- some type. ElementSymbolMap is an instance Functor, Treversable and Foldable
--- type classes.
+-- | An 'ElementSymbolMap' is a polymorphic datatype mapping an 'ElementSymbol'
+-- to some type. 'ElementSymbolMap' is an instance 'Functor', 'Treversable' and
+-- 'Foldable' type classes.
 newtype ElementSymbolMap a =
     ElementSymbolMap {getSymbolMap :: Map ElementSymbol a}
     deriving (Show, Read, Eq, Ord, Functor, Traversable, Foldable)
 
--- | Function to make an ElementSymbolMap.
+-- | Function to make an 'ElementSymbolMap'.
 mkElementSymbolMap :: [(ElementSymbol, a)] -> ElementSymbolMap a
 mkElementSymbolMap = ElementSymbolMap . fromList
 
--- | Polymorphic lookup function to get a value from an ElementSymbolMap.
+-- | Polymorphic lookup function to get a value from an 'ElementSymbolMap'.
 lookup :: ElementSymbol -> ElementSymbolMap a -> Maybe a
 lookup k m = Map.lookup k (getSymbolMap m)
 
--- | Polymorphic lookup function to get a value from an ElementSymbolMap.
+-- | Polymorphic lookup function to get a value from an 'ElementSymbolMap'.
 (!) :: ElementSymbolMap a -> ElementSymbol -> a
 m ! k = getSymbolMap m Map.! k
 
--- | Class containing three methods; monoisotopicMass, nominalMass and
--- averageMass.
+-- | Class containing three methods; 'monoisotopicMass', 'nominalMass' and
+-- 'averageMass'.
 class Mass a where
      monoisotopicMass :: a -> MonoisotopicMass
      nominalMass      :: a -> NominalMass
      averageMass      :: a -> AverageMass
 
--- | Element is an instance of Mass.
 instance Mass Element where
    monoisotopicMass = isotopicMass . elementMostAbundantIsotope
    nominalMass = massNumber . nucleons . elementMostAbundantIsotope
    averageMass e = sum [isotopicMass x * isotopicAbundance x |
                                 x <- isotopes' e]
 
--- | Returns the most abundant naturally-occurring isotope for an Element.
+-- | Returns the most abundant naturally-occurring isotope for an element.
 elementMostAbundantIsotope :: Element -> Isotope
 elementMostAbundantIsotope e = isotopeList !! indexOfIsotope
     where isotopeList = isotopes' e

00e0e86869603f60aed2446e5001781524ee0feb
Michaelt293 <Michaelt293@gmail.com>
2016-03-31 20:44:12 +1100

    Added support for molecular formulae

--- src/Isotope/Chemical.hs
+++ src/Isotope/Chemical.hs
@@ -1,13 +1,23 @@
+{-|
+Module      : Isotopic.Chemical
+Description :
+Copyright   : Michael Thomas
+License     : GPL-3
+Maintainer  : Michael Thomas <Michaelt293@gmail.com>
+Stability   : Experimental
+
+-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
 module Isotope.Chemical (
   -- * Type synonym
     ChemicalFormula
-  -- * Type class
-  , ChemicalFormulae(..)
+  -- * Type classes
+  , ToChemicalFormula(..)
+  , FormulaMult(..)
   -- * Parses
   , elementSymbol
   , chemicalFormula
@@ -18,6 +28,8 @@ module Isotope.Chemical (
   -- * Operators
   , (|+|)
   , (|-|)
+  , MolecularFormula
+  , renderMolecularFormula
   ) where
 
 import Prelude hiding (filter)
@@ -27,10 +39,10 @@ import Text.Megaparsec.String
 import qualified Text.Megaparsec.Lexer as L
 import Data.String
 import Isotope.Element
-import Isotope.Periodic
+import Isotope.Periodic()
 import Data.Map hiding (map, (!))
 
--- | ChemicalFormula is a type synonym for ElementSymbolMap Int.
+-- | 'ChemicalFormula' is a type synonym for @ElementSymbolMap Int@.
 type ChemicalFormula = ElementSymbolMap Int
 
 -- | Parses an element symbol string.
@@ -40,7 +52,7 @@ elementSymbol = read <$> choice (try . string <$> elementSymbolStrList)
           reverseLengthSort x y = length y `compare` length x
           elementSymbolStrList = sortBy reverseLengthSort elementList
 
--- | Parses an sub-formula (i.e., "C2").
+-- | Parses an sub-formula (i.e., \"C2\").
 subFormula :: Parser ChemicalFormula
 subFormula = do
     sym <- elementSymbol
@@ -49,14 +61,14 @@ subFormula = do
                   Nothing -> mkElementSymbolMap [(sym, 1)]
                   Just num' -> mkElementSymbolMap [(sym, fromIntegral num')]
 
--- | Parses a ChemicalFormula (i.e. "C6H6").
+-- | Parses a 'ChemicalFormula' (i.e. "C6H6").
 chemicalFormula :: Parser ChemicalFormula
 chemicalFormula = do
     formulas <- many subFormula
     return $ mconcat formulas
 
--- | An empty chemical formula, i.e., a formula with no atoms. This is mempty in
--- in the Monoid instance.
+-- | An empty chemical formula, i.e., a formula with no atoms. This is 'mempty'
+-- in the 'Monoid' instance.
 emptyFormula :: ChemicalFormula
 emptyFormula = mkElementSymbolMap []
 
@@ -67,13 +79,13 @@ renderFormula f = foldMapWithKey foldfunc (getSymbolMap f)
                                                               then ""
                                                               else show num
 
--- | IsString instance for ChemicalFormula. This allows the shorthand chemical
+-- | 'IsString' instance for 'ChemicalFormula'. This allows the shorthand chemical
 -- formula to be used directly in source code or in an interactive session.
 -- E.g., "C6H6" :: ChemicalFormula
 instance IsString ChemicalFormula where
     fromString s =
       case parse (chemicalFormula <* eof) "" s of
-           Left err -> error $ "Could not parse molecular formula: " ++ show err
+           Left err -> error $ "Could not parse chemical formula: " ++ show err
            Right v  -> v
 
 -- | ChemicalFormula is an instance of Monoid.
@@ -83,7 +95,7 @@ instance Monoid ChemicalFormula where
 
 -- | Class for types which can map to a chemical formula. This class has two
 -- methods; getFormula and getMaybeFormula.
-class ChemicalFormulae a where
+class ToChemicalFormula a where
     getFormula      :: a -> ChemicalFormula
     getMaybeFormula :: a -> Maybe ChemicalFormula
 
@@ -106,9 +118,8 @@ class FormulaMult a b c | a b -> c where
 -- key-value pairs with non-positive integers.
 combineSymbolMaps :: (Int -> Int -> Int) ->
                      ChemicalFormula ->  ChemicalFormula ->  ChemicalFormula
-combineSymbolMaps f m1 m2 = ElementSymbolMap $
-                                filter (>= 0) $ unionWith f (getSymbolMap m1)
-                                                            (getSymbolMap m2)
+combineSymbolMaps f m1 m2 = ElementSymbolMap $ unionWith f (getSymbolMap m1)
+                                                           (getSymbolMap m2)
 
 infixl 6 |+|
 infixl 7 |*|
@@ -126,8 +137,7 @@ instance FormulaMult Int ChemicalFormula ChemicalFormula where
 
 -- | Helper function for the multiplication of chemical formulae.
 multChemicalFormula :: ChemicalFormula -> Int ->  ChemicalFormula
-multChemicalFormula m n = ElementSymbolMap $
-                              filter (>= 0) $ (n *) <$> getSymbolMap m
+multChemicalFormula m n = ElementSymbolMap $ (n *) <$> getSymbolMap m
 
 -- | ChemicalFormula is an instance of Mass.
 instance Mass ChemicalFormula where
@@ -140,4 +150,43 @@ instance Mass ChemicalFormula where
 getFormulaSum :: (Num a, Integral b) =>
                  (ElementSymbol -> a) -> ElementSymbolMap b -> a
 getFormulaSum f m = sum $ mapWithKey mapFunc (getSymbolMap m)
-                        where mapFunc key val = f key * fromIntegral val
+                        where mapFunc k v = f k * fromIntegral v
+
+type MolecularFormula = [Either ChemicalFormula ([ChemicalFormula], Int)]
+
+instance Mass MolecularFormula where
+    monoisotopicMass = monoisotopicMass . getFormula
+    averageMass = averageMass . getFormula
+    nominalMass = nominalMass . getFormula
+
+instance ToChemicalFormula MolecularFormula where
+    getFormula = foldMap (\case
+        Left chemForm -> chemForm
+        Right (molForm, n) -> mconcat molForm |*| n)
+    getMaybeFormula x = Just (getFormula x)
+
+parenFormula :: Parser (Either ChemicalFormula ([ChemicalFormula], Int))
+parenFormula = do
+    char '('
+    formula <- some subFormula
+    char ')'
+    num <- optional L.integer
+    return $ Right $ case num of
+                          Nothing -> (formula, 1)
+                          Just num' -> (formula, fromIntegral num')
+
+leftChemicalFormula :: Parser (Either ChemicalFormula ([ChemicalFormula], Int))
+leftChemicalFormula = do
+    formula <- subFormula
+    return $ Left formula
+
+molecularFormula :: Parser MolecularFormula
+molecularFormula = some (leftChemicalFormula <|> parenFormula)
+
+instance IsString MolecularFormula where
+    fromString s =
+      case parse (molecularFormula <* eof) "" s of
+           Left err -> error $ "Could not parse molecular formula: " ++ show err
+           Right v  -> v
+
+renderMolecularFormula = undefined
